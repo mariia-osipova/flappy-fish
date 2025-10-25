4e550da35ce7eebee50bdfebe755d4af107adfee
Mariia Osipova <maryosipovaa@gmail.com>
2025-10-25 19:41:19 -0300

    centralize the square, add V

--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
 import pygame
+from jedi.debug import speed
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+h = 720
+w = 1280
+V = 1.3
+
+size = (150, 150)
+screen = pygame.display.set_mode((w, h))
 clock = pygame.time.Clock()
 running = True
 
-player_rect = pygame.Rect(100, 100, 50, 50)
+player_image = pygame.image.load('../data/img/img_1.png').convert_alpha()
+player_image = pygame.transform.scale(player_image, (size[0], size[1]))
+
+# player_rect = pygame.Rect(w//2 - size[0]//2, h//2 - size[1]//2, size[0], size[1])
+
+player_rect = player_image.get_rect(center=(w//2, h//2))
 
 while running:
     for event in pygame.event.get():
@@ -14,20 +25,15 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_rect.y -= 5
+        player_rect.y -= 5*V
     if keys[pygame.K_s]:
-        player_rect.y += 5
-    if keys[pygame.K_a]:
-        player_rect.x -= 5
-    if keys[pygame.K_d]:
-        player_rect.x += 5
+        player_rect.y += 5*V
+    if not keys[pygame.K_w]:
+        player_rect.y += 5*(V+0.7)
 
     screen.fill("skyblue3")
-
-    pygame.draw.rect(screen, (0, 0, 0), player_rect)
-
+    screen.blit(player_image, player_rect)
     pygame.display.flip()
-
     clock.tick(60)
 
 pygame.quit()
